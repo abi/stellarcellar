f73844a3c2264b1df4db4750cb151a2f9e2a785d
Abi Raja <abimanyuraja@gmail.com>
2014-04-19 22:49:02 -0700

    stop using jquery

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,25 @@
-$(document).ready(function () {
-  // Events handlers
-  $('#logout').click(function () {
-    $.post('/logout', {_csrf: _csrf}, function () {
-      window.location = '/'
-    })
-  })
-})
+document.addEventListener('DOMContentLoaded', function (evt) {
+
+  var logoutButton = document.querySelector('#logout');
+  if (logoutButton) {
+    logoutButton.addEventListener('click', function () {
+      var req = new XMLHttpRequest();
+      req.open('POST', '/logout');
+      req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+      req.addEventListener('load', function () {
+        if (req.status === 200) {
+          window.location = '/';
+        } else {
+          window.location = '/';
+        }
+      }, false);
+
+      req.addEventListener('error', function () {
+        window.location = '/';
+      }, false);
+
+      req.send(encodeURIComponent('_csrf') + '=' + encodeURIComponent(_csrf));
+    });
+  }
+
+});
